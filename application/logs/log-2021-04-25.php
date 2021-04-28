09abd4531d7afe9ac990e3b2f884bc8e090325ec
unknown <pawangntd@gmail.com>
2021-04-28 12:57:52 +0530

    json file

---
+++ application/logs/log-2021-04-25.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-04-25 12:06:55 --> 404 Page Not Found: /index
+ERROR - 2021-04-25 12:06:56 --> 404 Page Not Found: /index
+ERROR - 2021-04-25 12:06:56 --> 404 Page Not Found: /index
